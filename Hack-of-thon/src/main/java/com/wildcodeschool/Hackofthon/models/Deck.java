736d9842a82c19eee82d4e7d33ddfdd54c329a41
Adrien MINICILLI <adrien.minicilli@gmail.com>
2019-10-30 18:28:56 +0100

    Added Deck JSON

--- Hack-of-thon/src/main/java/com/wildcodeschool/Hackofthon/models/Deck.java
+++ Hack-of-thon/src/main/java/com/wildcodeschool/Hackofthon/models/Deck.java
@@ -1,33 +1,60 @@
 package com.wildcodeschool.Hackofthon.models;
 
-import java.io.IOException;
-import java.util.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-public class Deck{
-	
-	private Card card;
-	private ArrayList<Card> cardsDeck;
-	
-	public Deck() {
 		
-		String url = "https://hackathon-wild-hackoween.herokuapp.com/monsters";
-		WebClient webClient = WebClient.create(url);
-		Mono<String> call = webClient.get().retrieve().bodyToMono(String.class);
-		String response = call.block();
-		ObjectMapper objectMapper = new ObjectMapper();
-		try {
-		    JsonNode root = objectMapper.readTree(response);	    
-		    for (int i = 0 ; i < root.size() ; i++) {
-		    	card = objectMapper.convertValue(root.get(i), Card.class);
-		    	this.cardsDeck.add(card);
-		    }
-		} 
-		catch (IOException e) {
-			e.printStackTrace();
+public class Deck {
+			
+
+		static String url = "https://hackathon-wild-hackoween.herokuapp.com/monsters";
+		private Card[] cardsList;
+		
+		private static Card[] createCards() {
+			
+			WebClient webClient = WebClient.create(url);
+			Mono<String> call = webClient.get()
+				      .retrieve()
+			      .bodyToMono(String.class);
+			String response = call.block();
+			ObjectMapper objectMapper = new ObjectMapper();
+
+			
+
+			try {
+				JsonNode root = objectMapper.readTree(response);
+
+				} 
+				catch (IOException e) {
+				   e.printStackTrace();
+
+				}
+			
+			Card[] cards = null;
+
+			try {
+
+				JsonNode monster = objectMapper.readTree(response).get("monsters");
+
+			   cards = objectMapper.convertValue(monster, Card[].class);
+			} 
+			
+			catch (IOException e) {
+
+			   e.printStackTrace();
+
+			}
+			
+			return cards;
+
 		}
-	}
+
+
 }
+
