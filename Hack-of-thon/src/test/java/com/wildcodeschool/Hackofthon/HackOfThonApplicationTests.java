27917a19a4179aa36f1835b60618a7da4a3f4c72
52-28 <aymeric.feillault@gmail.com>
2019-10-30 12:05:18 +0100

    initialisation projet springBoot

---
+++ Hack-of-thon/src/test/java/com/wildcodeschool/Hackofthon/HackOfThonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.Hackofthon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackOfThonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
