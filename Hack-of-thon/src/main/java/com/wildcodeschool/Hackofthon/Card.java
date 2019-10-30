3e9dc583623c2dcc28166de59181f9feafe2c152
Adrien MINICILLI <adrien.minicilli@gmail.com>
2019-10-30 16:10:17 +0100

    added Card.java, Deck.java

---
+++ Hack-of-thon/src/main/java/com/wildcodeschool/Hackofthon/Card.java
@@ -0,0 +1,138 @@
+package com.wildcodeschool.Hackofthon;
+
+
+public class Card() {
+	
+	private int id;
+	private String name;
+	private String level;
+	private int attack;
+	private int defense;
+	private int life = 0;
+	private String special;
+	private String description;
+	private String picture;
+	
+	
+	public Card(int id, String name, String level, int attack, int defense, int life, String special, String description, String picture) {
+		
+		this.id = id;
+		this.name = name;
+		this.level = level;
+		this.attack = attack;
+		this.defense = defense;
+		this.life = life;
+		this.special = special;
+		this.description = description;
+		this.picture = picture;
+	}
+	
+	
+	public int getId() {
+		
+		return this.id;
+	}
+	
+	
+	public int getName() {
+			
+		return this.name;
+	}
+	
+	
+	public String getlevel() {
+		
+		return this.level;
+	}
+	
+	
+	public int getAttack() {
+		
+		return this.attack;
+	}
+	
+	
+	public int getDefense() {
+		
+		return this.defense;
+	}
+	
+	
+	public int getLife() {
+		
+		return this.life;
+	}
+	
+	
+	public String getSpecial() {
+		
+		return this.special;
+	}
+	
+	
+	public String getDescription() {
+			
+		return this.description;
+	}
+	
+	
+	public String getPicture() {
+		
+		return this.getPicture;
+	}
+	
+	
+	public void setId(int id) {
+		
+		this.id = id;
+	}
+	
+	
+	public void setName(int name) {
+		
+		this.name = name;
+	}
+	
+	
+	public void setLevel(String level) {
+		
+		this.level = level;
+	}
+	
+	
+	public void setAttack(int attack) {
+		
+		this.attack = attack;
+	}
+	
+	
+	public void setDefense(int defense) {
+		
+		this.defense = defense;
+	}
+	
+	
+	public void setLife(int life) {
+		
+		this.life = life;
+	}
+	
+	public void setSpecial(String special) {
+		
+		this.special = special;
+	}
+	
+	
+	public void setDescription(String description) {
+		
+		this.description = description;
+	}
+	
+	
+	public void setPicture(String picture) {
+		
+		this.id = id;
+	}	
+	
+}
+
---
+++ Hack-of-thon/src/main/java/com/wildcodeschool/Hackofthon/Deck.java
@@ -0,0 +1,23 @@
+package com.wildcodeschool.Hackofthon;
+
+public class Deck{
+	
+	private Card card;
+	private ArrayList<Card> cardsDeck;
+	private final static String JSON_MONSTERS_PATH = "monsters.json";
+	
+	public deck() {
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		try {
+		    JsonNode root = objectMapper.readTree(new File(JSON_MONSTERS_PATH));	    
+		    for (int i = 0 ; i < root.size() ; i++) {
+		    	card = objectMapper.convertValue(root.get(i), Card.class);
+		    	this.cardsDeck.add(card);
+		    }
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+}
