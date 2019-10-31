be0fccdf9f5f6f1c191b817da63b3bc011f2ced7
Clement1987 <jaminclement1987@gmail.com>
2019-10-31 10:00:59 +0100

    gif implémentation

--- Hack-of-thon/src/main/resources/static/board.js
+++ Hack-of-thon/src/main/resources/static/board.js
@@ -10,4 +10,39 @@ function togglePlay() {
 		pause.innerHTML = "🔊";
 		pause.style.color = " #848484";
 	}
-}
+};
+
+document.querySelector('#gif1').addEventListener("click",function(event){
+	document.querySelector("#gif").style.display = 'block';
+	setTimeout(() => {
+		window.location.href = "/board2?idMonster=1"
+	}, 3000)
+});
+
+document.querySelector('#gif2').addEventListener("click",function(event){
+	document.querySelector("#gif").style.display = 'block';
+	setTimeout(() => {
+		window.location.href = "/board2?idMonster=2"
+	}, 3000)
+});
+
+document.querySelector('#gif3').addEventListener("click",function(event){
+	document.querySelector("#gif").style.display = 'block';
+	setTimeout(() => {
+		window.location.href = "/board2?idMonster=3"
+	}, 3000)
+});
+
+document.querySelector('#gif4').addEventListener("click",function(event){
+	document.querySelector("#gif").style.display = 'block';
+	setTimeout(() => {
+		window.location.href = "/board2?idMonster=4"
+	}, 3000)
+});
+
+document.querySelector('#gif5').addEventListener("click",function(event){
+	document.querySelector("#gif").style.display = 'block';
+	setTimeout(() => {
+		window.location.href = "/board2?idMonster=5"
+	}, 3000)
+});
